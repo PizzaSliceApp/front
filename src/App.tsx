2e95c74d0da3319c44880a73f4079df5a2621ba6
BrunoUA96 <ostapklysa@gmail.com>
2023-10-20 17:03:45 +0100

    add simple header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+import { Header } from '@common/components/header.components';
+
 export const App = () => {
   return (
     <>
+      <Header />
       <h1 className="">🍕 Pizza</h1>
     </>
   );
---
+++ src/common/components/header.components.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+  return (
+    <header className="h-12 shadow-lg flex items-center px-6">
+      <div className="text-xl font-bold">🍕 Pizza Slice</div>
+    </header>
+  );
+};
