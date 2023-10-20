e8236f67a27b9a03ee1d7192a5fa1a8c72f6e93c
BrunoUA96 <ostapklysa@gmail.com>
2023-10-20 22:03:08 +0100

    add main wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,19 @@ import pizzaMenu from '@app/mocks/pizza.json';
 
 import { MenuList } from '@modules/menu/components/menu-list/menu-list.component';
 
-import { Header } from '@common/components/header.components';
+import { Footer } from '@common/components/footer.component';
+import { Header } from '@common/components/header.component';
 
 export const App = () => {
   return (
     <>
       <Header />
 
-      <MenuList items={pizzaMenu} />
+      <main className="mt-12 mb-24 mx-10">
+        <MenuList items={pizzaMenu} />
+      </main>
+
+      <Footer />
     </>
   );
 };
